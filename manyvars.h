3bb68bc7d27d01ecd0b3bc60a8b29cb168e79ad2
MattCaner <maciejftrzcinski@gmail.com>
2018-04-12 22:14:04 +0200

    Create manyvars.h

---
+++ manyvars.h
@@ -0,0 +1,14 @@
+#ifndef _MANYVARS_H
+#define _MANYVARS_H
+#include "mathparser.h"
+
+/* Why is six afraid of seven?
+ * It is not. Numbers are not sentient and thus incapable of feeling fear.
+ * 
+*/
+
+struct eValue;
+
+float autoDiff(struct expr *, int, ...);
+
+#endif
